c8fd1c2ff98c052e771c8a408b29244dc59d50e9
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2024-08-09 20:31:59 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # BlockPicking
+
+## Installation
+
+1.```pip install -r requirements.txt``` \
+2.```cd xArm-Python-SDK``` then,
+```python setup.py install```
+
+## Usage
+1. Run test_movement.ipynb to test if the robot is connected and can move.
+2. Run coord_calibration.ipynb to calibrate the camera and robot coordinate system.
+3. Run live_detection.ipynb to start the picking up process.
---
+++ utils.py
@@ -0,0 +1,56 @@
+from xarm.wrapper import XArmAPI
+from time import sleep
+
+
+class ArmHandler:
+    def __init__(self):
+        """
+        Initialize the arm (Try setting your ethernet ip to 192.168.1.225)
+        """
+        self.arm = XArmAPI('192.168.1.155')
+        self.arm.motion_enable(enable=True)
+        self.arm.set_mode(0)
+        self.arm.set_state(0)
+        self.arm.connect()
+        self.arm.move_gohome()
+
+    def place_block_on(self, x=225, y=-95, z=87):
+        """
+        Drop the block at the given position, by default at assumed conveyor belt position
+        :param x:
+        :param y:
+        :param z:
+        :return:
+        """
+        self.arm.move_gohome()
+        self.arm.set_position(x, y, z, roll=180, pitch=0, yaw=0)
+        self.arm.set_suction_cup(False)
+        self.arm.set_position(x=x, y=y, z=z + 10, roll=180, pitch=0, yaw=0)
+        self.arm.move_gohome()
+
+    def pickup_block_from(self, x, y, z=20) -> bool:
+        """
+        :param x:
+        :param y:
+        :param z:
+        :return: Returns false if out of bounds, else True
+        """
+        if not (215 < x < 300) or y < 0:
+            print("Out of RANGE")
+            return False
+
+        self.arm.set_suction_cup(True)
+        self.arm.set_position(x=x, y=y, z=z + 10, roll=180, pitch=0, yaw=0)
+        sleep(0.5)
+        self.arm.set_position(x=x, y=y, z=z, roll=180, pitch=0, yaw=0)
+        sleep(1)
+        self.arm.set_position(x=x, y=y, z=z + 40, roll=180, pitch=0, yaw=0)
+        self.arm.move_gohome()
+
+        return True
+
+    def disconnect(self):
+        self.arm.set_suction_cup(False)
+        sleep(2)
+        self.arm.move_gohome()
+        self.arm.disconnect()
